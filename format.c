34c72af5015446509e7781d0047115d1ede4eea2
schnitgurl lalallala <146375954+schnitGurl@users.noreply.github.com>
2024-06-19 13:08:05 -0400

    Delete format.c

--- format.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv)
-{
-    char *items[] = {"boat", "car", "truck", "train"};
-    int index = atoi(argv[1]);
-    printf("You selected %s\n", items[index-1]);
-}
